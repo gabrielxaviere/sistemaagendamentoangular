a3f98f756a34b7c9fa8f01d759bfbfd8caf52491
Gabriel Xavier de Oliveira <gabrielxaviere@gmail.com>
2023-12-18 20:44:26 -0300

    adionar range de data
    -desabilitar sabados e domingos

--- src/app/shared/components/datetime/datetime.component.ts
+++ src/app/shared/components/datetime/datetime.component.ts
@@ -10,11 +10,17 @@ export class DatetimeComponent {
   datetimeForm: FormGroup;
   hours: number[];
   minutes: number[];
+  minDate: Date;
+  maxDate: Date;
   @Input() disabledDates: Date[] = [];
 
   @Output() datetimeSelected = new EventEmitter<Date>();
 
   constructor(private fb: FormBuilder) {
+    this.minDate = new Date();
+    this.maxDate = new Date();
+    this.maxDate.setMonth(this.maxDate.getMonth() + 2);
+
     this.hours = this.generateRange(8, 18);
     this.minutes = this.generateRange(0, 50, 10);
 
@@ -23,6 +29,14 @@ export class DatetimeComponent {
       hour: [8, Validators.required],
       minute: [0, Validators.required],
     });
+
+    console.log(this.minDate)
+  }
+
+  dateFilter = (date: Date | null): boolean => {
+    const day = date.getDay();
+
+    return day !== 0 && day !== 6;
   }
 
   generateRange(start: number, end: number, step: number = 1): number[] {
@@ -52,29 +66,28 @@ export class DatetimeComponent {
     if (!Array.isArray(this.disabledDates)) {
       return false; // Permite o campo se nenhuma data corresponder
     }
-  
+
     const isNullOrInvalidDate = (d: Date) => d == null || !(d instanceof Date) || isNaN(d.getTime());
-  
+
     return this.disabledDates.some(disabledDate => {
-      try {  
-      
+      try {
+
         const combinedDateTime = new Date(date);
         combinedDateTime.setHours(hour);
         combinedDateTime.setMinutes(minute);
 
         const parsedDate = new Date(disabledDate);
-  
+
         var a = parsedDate.getTime() === combinedDateTime.getTime();
 
-        if(a == true)
-        {
-          console.log("Data correta"+parsedDate)
-          console.log("Data correta"+combinedDateTime)
-          return true; 
-        }else{
+        if (a == true) {
+          console.log("Data correta" + parsedDate)
+          console.log("Data correta" + combinedDateTime)
+          return true;
+        } else {
           return false;
         }
-        
+
       } catch (error) {
         // Emitir um evento ou lançar uma exceção para lidar com o erro
         console.error(`Erro ao processar data desabilitada: ${error}`);
